0421a619ca3d81223d97f02cc4de3a3428c3b68a
jonmanahan <jmanahan@8thlight.com>
2022-05-16 11:53:26 -0400

    Added test coverage to make sure communicate was called

--- app/src/main/java/waiter/Communicator/mock/CommunicatorMock.java
+++ app/src/main/java/waiter/Communicator/mock/CommunicatorMock.java
@@ -6,9 +6,24 @@ import waiter.Reactor.Reactive;
 
 import java.io.IOException;
 
-public record CommunicatorMock(IOException exception) implements Reportable {
+public class CommunicatorMock implements Reportable {
+
+    private final boolean throwException;
+    private final IOException exception;
+    public boolean communicateWasCalled;
+
+    public CommunicatorMock(boolean throwException, IOException exception) {
+        this.communicateWasCalled = false;
+        this.throwException = throwException;
+        this.exception = exception;
+    }
 
     public void communicate(Reactive reactive) throws IOException {
-        throw this.exception;
+        if(throwException) {
+            throw this.exception;
+        }
+        else {
+            this.communicateWasCalled = true;
+        }
     }
 }
--- app/src/test/java/waiter/EchoServerTest.java
+++ app/src/test/java/waiter/EchoServerTest.java
@@ -5,13 +5,14 @@ import waiter.Communicator.mock.CommunicatorMock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
 
 import java.io.IOException;
 import java.net.SocketException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class EchoServerTest {
 
@@ -30,15 +31,28 @@ class EchoServerTest {
 
     @Test
     void catchesIOException() {
-        EchoServer echoServer = new EchoServer(new CommunicatorMock(new IOException()));
+        CommunicatorMock communicatorMock = new CommunicatorMock(true, new IOException());
+        EchoServer echoServer = new EchoServer(communicatorMock);
         echoServer.start(4424);
         assertEquals("Sorry, an error occurred when sending/receiving a message, please try running the server and connecting again", output.toString().trim());
+        assertFalse(communicatorMock.communicateWasCalled);
     }
 
     @Test
     void catchesSocketException() {
-        EchoServer echoServer = new EchoServer(new CommunicatorMock(new SocketException()));
+        CommunicatorMock communicatorMock = new CommunicatorMock(true, new SocketException());
+        EchoServer echoServer = new EchoServer(communicatorMock);
         echoServer.start(4424);
         assertEquals("Sorry, connection could not be established or has been broken, please try running the server and connecting again", output.toString().trim());
+        assertFalse(communicatorMock.communicateWasCalled);
+    }
+
+    // This is to cover lack of integration or E2E tests, could be replaced when these test type coverages are introduced
+    @Test
+    void callsCommunicate() {
+        CommunicatorMock communicatorMock = new CommunicatorMock(false, new IOException());
+        EchoServer echoServer = new EchoServer(communicatorMock);
+        echoServer.start(4425);
+        assertTrue(communicatorMock.communicateWasCalled);
     }
 }
