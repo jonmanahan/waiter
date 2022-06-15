822a9f958063a76f7c9ef1bb320814ab23f67a71
jonmanahan <99982874+jonmanahan@users.noreply.github.com>
2022-06-15 15:19:49 -0400

    Circle ci setup
    * Add circleci initial files
    
    * Modify http server spec
    
    * Update config yaml to run unit and acceptance tests
    
    * Purposefully running failing test for verification
    
    * Fix Router to have default error routes, verify fail acceptance test
    
    * Fix acceptance test ran to not fail, now verify circleci fail unitTest
    
    * Fix unit tests to pass again
    
    * Combine unit test and build step since gradle build runs the tests
    
    * Update http server spec submodule

--- app/src/main/java/waiter/Router.java
+++ app/src/main/java/waiter/Router.java
@@ -4,12 +4,21 @@ import java.util.Map;
 
 public class Router {
 
-    Map<String, Map<String, String>> routes;
+    private final Map<String, Map<String, Map<String, String>>> routes;
+    private final String noMethod = "No Method";
+    private final String noUrl = "No Url";
 
     public Router() {
         this.routes = Map.ofEntries(
-                Map.entry("GET /simple_get", setResponseFields("200 OK", "Content-Length:", "")),
-                Map.entry("GET /simple_get_with_body", setResponseFields("200 OK", "Content-Length:", "Hello world"))
+                Map.entry("GET", Map.ofEntries(
+                        Map.entry("/simple_get", setResponseFields("200 OK", "Content-Length:", "")),
+                        Map.entry("/simple_get_with_body", setResponseFields("200 OK", "Content-Length:", "Hello world")),
+                        //This will become a different response when Method Not Allowed is implemented
+                        Map.entry(noUrl, setResponseFields("500 Internal Server Error", "Content-Length:", ""))
+                )),
+                Map.entry(noMethod, Map.ofEntries(
+                        Map.entry(noUrl, setResponseFields("500 Internal Server Error", "Content-Length:", ""))
+                ))
         );
     }
 
@@ -24,8 +33,9 @@ public class Router {
 
     public Route getRoute(Request request) {
 
-        String methodWithURI = request.getMethod() + " " + request.getUrl();
-        Map<String, String> requestResult = routes.get(methodWithURI);
+        Map<String, Map<String, String>> methodAssociatedUrls = routes.getOrDefault(request.getMethod(), routes.get(noMethod));
+        Map<String, String> requestResult = methodAssociatedUrls.getOrDefault(request.getUrl(), methodAssociatedUrls.get(noUrl));
+
         return new Route(request.getProtocol(), requestResult.get("status"), requestResult.get("headers"), requestResult.get("body"));
     }
 }
