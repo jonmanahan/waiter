0e4c1270ec780a01d64a46ee19e5cb2564ba1a35
jonmanahan <jmanahan@8thlight.com>
2022-07-01 10:41:21 -0400

    Move private parsing functions from ClientConnection to RequestParser

--- app/src/main/java/waiter/RequestParser.java
+++ app/src/main/java/waiter/RequestParser.java
@@ -2,10 +2,14 @@ package waiter;
 
 public class RequestParser {
 
+    private static int currentRequestBodyLength;
+
     public Request parse(String requestMessage) {
+        System.out.println(requestMessage);
 
         String endOfStartLineDelimiter = "\r\n";
         String endOfHeadersDelimiter = "\r\n\r\n";
+        System.out.println(requestMessage.indexOf(endOfStartLineDelimiter));
 
         String requestStartLine = requestMessage.substring(
                 0, getIndexAtDelimiter(requestMessage, endOfStartLineDelimiter)
@@ -34,5 +38,40 @@ public class RequestParser {
 
     private int getIndexAtDelimiter(String requestMessage, String delimiter) {
         return requestMessage.indexOf(delimiter);
+    }
+
+    public static boolean notEndOfRequest(StringBuilder requestMessageBuilder) {
+        int requestBodyLength = 0;
+
+        if(headersWereRead(requestMessageBuilder)) {
+            requestBodyLength = getContentLength(requestMessageBuilder);
+            currentRequestBodyLength += 1;
+        }
+        else {
+            currentRequestBodyLength = 0;
+        }
+
+        return currentRequestBodyLength <= requestBodyLength;
+    }
+
+    private static int getContentLength(StringBuilder requestMessageBuilder) {
+        int contentLength = 0;
+        String[] splitRequestMessage = requestMessageBuilder.toString().split("Content-Length: ");
+
+        if(contentLengthExists(splitRequestMessage)) {
+            contentLength = Integer.parseInt(
+                    splitRequestMessage[1].substring(0, splitRequestMessage[1].indexOf("\r\n"))
+            );
+        }
+
+        return contentLength;
+    }
+
+    private static boolean contentLengthExists(String[] contentLengthHeader) {
+        return contentLengthHeader.length == 2;
+    }
+
+    private static boolean headersWereRead(StringBuilder requestMessageBuilder) {
+        return requestMessageBuilder.indexOf("\r\n\r\n") != -1;
     }
 }
